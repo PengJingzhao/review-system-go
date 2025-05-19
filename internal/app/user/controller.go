c18a1765608b3561144f081716bc7d454516d8b5
PengJingzhao <1670916827@qq.com>
2025-05-19 10:59:33 +0800

    feat(user): 新增用户详情，用户添加功能

--- internal/app/user/controller.go
+++ internal/app/user/controller.go
@@ -3,11 +3,14 @@ package user
 import (
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"strconv"
 )
 
 func RegisterRoutes(r *gin.Engine) {
 	group := r.Group("/users")
 	group.GET("/listUsers", listUsers)
+	group.GET("/getUser/:id", getUser)
+	group.POST("/createUser", createUser)
 }
 
 func listUsers(c *gin.Context) {
@@ -17,4 +20,29 @@ func listUsers(c *gin.Context) {
 		return
 	}
 	c.JSON(http.StatusOK, users)
+}
+
+func getUser(c *gin.Context) {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+	}
+	user, err := GetUser(uint(id))
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, user)
+}
+
+func createUser(c *gin.Context) {
+	var user User
+	if err := c.ShouldBindJSON(&user); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+	}
+	err := CreateUser(&user)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+	}
+	c.JSON(http.StatusOK, gin.H{"userId": user.ID})
 }
--- internal/app/user/service.go
+++ internal/app/user/service.go
@@ -7,3 +7,21 @@ func ListUsers() ([]User, error) {
 	err := db.DB.Find(&users).Error
 	return users, err
 }
+
+func GetUser(id uint) (*User, error) {
+	var user User
+	err := db.DB.First(&user, id).Error
+	return &user, err
+}
+
+func CreateUser(u *User) error {
+	return db.DB.Create(u).Error
+}
+
+func UpdateUser(u *User) error {
+	return db.DB.Save(u).Error
+}
+
+func DeleteUser(id uint) error {
+	return db.DB.Delete(&User{}, id).Error
+}
